a36e80b2949e5c2d5768b860fbbad5d21182f3b1
Ogbonna Chibuoyim <ogbonnachibuoyim12@gmail.com>
2020-12-10 20:57:11 +0100

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,11 +1,11 @@
 CREATE TABLE Admin (
     ID int PRIMARY KEY AUTO_INCREMENT,
     Username varchar(255) NOT NULL,
-	Password varchar(255) NOT NULL
+    Password varchar(255) NOT NULL
 );
 
 CREATE TABLE Puzzle (
-	ID int Primary KEY AUTO_INCREMENT, 
+    ID int Primary KEY AUTO_INCREMENT, 
     Image1 VARCHAR(255) NOT NULL, 
     Image2 VARCHAR(255) NOT NULL, 
     Image3 VARCHAR(255) NOT NULL, 
